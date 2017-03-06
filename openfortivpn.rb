1bb7e4b79862a353f040f550ea2183e9795145c9
Emmanuel Blot <eblot.com@gmail.com>
2017-03-06 23:16:32 +0100

    Use a tagged version, as HEAD keeps fluctuating with Linux-focused development that regularly breaks macOS builds

--- openfortivpn.rb
+++ openfortivpn.rb
@@ -2,7 +2,9 @@ class Openfortivpn < Formula
   desc "Client for PPP+SSL Fortinet VPN tunnel services."
 
   homepage "https://github.com/adrienverge/openfortivpn"
+  url "https://github.com/adrienverge/openfortivpn/archive/v1.3.0.tar.gz"
   head "https://github.com/adrienverge/openfortivpn.git"
+  sha256 "a7dee87a9ef56c5d5a5d7288ae047f51f29472b2156e7d59bf9301aad6ac44ce"
 
   depends_on "automake" => :build
   depends_on "autoconf" => :build
