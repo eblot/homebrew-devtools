6e80f9915a7efc5c74481c6ecf1c0921e10e7495
Emmanuel Blot <emmanuel.blot@free.fr>
2018-07-06 11:40:52 +0200

    Update OpenOCD for nRF52 with UICR flash support and flash-with-watchdog

--- openocd-nrf52.rb
+++ openocd-nrf52.rb
@@ -3,30 +3,65 @@ class OpenocdNrf52 < Formula
   homepage "https://sourceforge.net/projects/openocd/"
 
   head do
-    url "https://github.com/eblot/openocd.git", :branch => "nrf52"
+    url "https://github.com/eblot/openocd.git", :branch => "nrf52-wdog"
 
     depends_on "autoconf" => :build
     depends_on "automake" => :build
     depends_on "libtool" => :build
     depends_on "texinfo" => :build
+    depends_on "pkg-config" => :build
+
+    depends_on "libusb"
+    depends_on "libftdi"
+    depends_on "hidapi"
   end
 
-  option "without-hidapi", "Disable building support for devices using HIDAPI (CMSIS-DAP)"
-  option "without-libftdi", "Disable building support for libftdi-based drivers (USB-Blaster, ASIX Presto, OpenJTAG)"
-  option "without-libusb",  "Disable building support for all other USB adapters"
-
-  depends_on "pkg-config" => :build
-  depends_on "libusb"
-  depends_on "libftdi"
-  depends_on "hidapi"
-
   def install
-    # all the libusb and hidapi-based drivers are auto-enabled when
-    # the corresponding libraries are present in the system
     args = %W[
-      --disable-dependency-tracking
       --prefix=#{prefix}
+      --enable-verbose
+      --enable-verbose-jtag-io
+      --enable-verbose-usb-io
+      --enable-verbose-usb-comms
       --enable-ftdi
+      --enable-cmsis-dap
+      --enable-jlink
+      --disable-doxygen-html
+      --disable-doxygen-pdf
+      --disable-werror
+      --disable-dummy
+      --disable-stlink
+      --disable-ti-icdi
+      --disable-ulink
+      --disable-usb-blaster-2
+      --disable-ft232r
+      --disable-vsllink
+      --disable-xds110
+      --disable-osbdm
+      --disable-opendous
+      --disable-aice
+      --disable-usbprog
+      --disable-rlink
+      --disable-armjtagew
+      --disable-kitprog
+      --disable-usb-blaster
+      --disable-presto
+      --disable-openjtag
+      --disable-parport
+      --disable-jtag_vpi
+      --disable-amtjtagaccel
+      --disable-zy1000-master
+      --disable-zy1000
+      --disable-ep93xx
+      --disable-at91rm9200
+      --disable-bcm2835gpio
+      --disable-imx_gpio
+      --disable-gw16012
+      --disable-oocd_trace
+      --disable-buspirate
+      --disable-sysfsgpio
+      --disable-minidriver-dummy
+      --disable-remote-bitbang
     ]
 
     ENV["CCACHE"] = "none"
